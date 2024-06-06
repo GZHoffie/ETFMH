be228e57932f66331aff5c735b313b443b8a7d10
Benson Lin <19587330+bensonlzl@users.noreply.github.com>
2024-06-06 17:31:57 +0800

    Added Prev1MutationModel to mutation_model.py
    Rearranged code such that both Prev1MutationModel and RandomMutationModel inherit from TrueFalseMutationModel

--- model/mutation_model.py
+++ model/mutation_model.py
@@ -1,13 +1,27 @@
 import numpy as np
 
 class MutationModel:
+    """
+    Base class for a mutation model 
+
+    Contains a ground truth 
+    """
     def __init__(self) -> None:
         pass
 
     def containment(self, k):
         pass
 
-class RandomMutationModel(MutationModel):
+class TrueFalseMutationModel(MutationModel):
+    """
+    Ground truth represented as a numpy array of True/False values 
+    Containment computed by taking k-mers across the ground truth using a sliding window
+    """
+    def containment(self, k):
+        kmer_matching = np.all(np.lib.stride_tricks.sliding_window_view(self.ground_truth, window_shape=k), axis=1)
+        return kmer_matching.sum() / len(kmer_matching)
+
+class RandomMutationModel(TrueFalseMutationModel):
     """
     Assume mutation is distributed uniformly at random in the read, and
     all k-mers are unique. How will the containment index change?
@@ -17,15 +31,44 @@ class RandomMutationModel(MutationModel):
         self.p = 1 - ANI
         self.ground_truth = np.random.choice(a=[False, True], size=length, p=[self.p, 1-self.p])
     
-    def containment(self, k):
-        kmer_matching = np.all(np.lib.stride_tricks.sliding_window_view(self.ground_truth, window_shape=k), axis=1)
-        return kmer_matching.sum() / len(kmer_matching)
+
+class Prev1MutationModel(TrueFalseMutationModel):
+    """
+    p0 : Probability that the current base will mutate if the previous base does not mutate
+    p1 : Probability that the current base will not mutate if the previous base mutated
+
+    In steady state, ANI = (1 - p1) / (1 - p1 + p0)
+    """
+    def __init__(self, p0, p1, length) -> None:
+        super().__init__()
+        self.ani = (1 - p1) / (1 - p1 + p0)
+
+        prev_mut = True
+        self.ground_truth = []
+        for _ in range(length):
+            if prev_mut:
+                self.ground_truth.append(np.random.choice(a=[False, True],p=[p0, 1-p0]))
+            else:
+                self.ground_truth.append(np.random.choice(a=[False, True],p=[p1, 1-p1]))
+            prev_mut = self.ground_truth[-1]
+
+        self.ground_truth = np.array(self.ground_truth)
+        self.ground_ani = (self.ground_truth * 1).sum() / len(self.ground_truth)
+
 
 
 if __name__ == "__main__":
-    m = RandomMutationModel(0.9, 1000)
-    for i in range(1, 51):
+    ANI = 0.90
+    LENGTH = 1000000
+    KMAX = 60
+
+    m = RandomMutationModel(ANI, LENGTH)
+    for i in range(1, KMAX + 1):
         containment = m.containment(i)
         print(i, containment, containment ** (1/i))
 
         
+    pm = Prev1MutationModel(0.10, 0.9, LENGTH)
+    for i in range(1, KMAX + 1):
+        containment = pm.containment(i)
+        print(i, pm.ani, pm.ground_ani, containment, containment ** (1/i))
