abaea7fd2739e5d97803d0d8f0bdafd77f05905d
Gu Zhenhao <guzh@comp.nus.edu.sg>
2024-06-10 11:13:02 +0800

    Dataset downloading script

--- dataset/dataset_download.py
+++ dataset/dataset_download.py
@@ -13,18 +13,17 @@ class ReferenceGenomeDownloader:
     """
     A utility class enabling us to download reference genomes from NCBI RefSeq database.
     """
-    def __init__(self, metadata_df) -> None:
-        """
-        Args:
-            - metadata_df (pd.DataFrame): A dataframe consisting of GTDB metadata.
-        """
-        self.metadata_df = metadata_df
+    def __init__(self) -> None:
+        pass
     
     def _download_reference(self, accession, directory, name):
         """
         Download the reference using the given accession number, and store it in
         ${directory}/${name}.fna. If the file already exists, we append to the file.
         """
+        if not os.path.isabs(directory):
+            print("[WARNING]\tThe path is not an absolute path.")
+
         # mkdir if the directory doesn't exist
         Path(directory).mkdir(parents=True, exist_ok=True)
 
@@ -50,53 +49,55 @@ class ReferenceGenomeDownloader:
         subprocess.run(["rm", "-r", directory], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 
-    def find_signature(self, accession_list, taxid_list, tax_id):
+    def download_reference_from_acession_list(self, accession_list, directory):
         """
-        Given a list of accession, determine the signature.
+        Given a list of accession, Download the corresponding reference under directory.
         """
-        # Download the reference corresponding to the accession
-        directory = "/home/zhenhao/TDT/data_temp/" + str(tax_id)
-
-        for i, accession in tqdm(enumerate(accession_list), desc="Downloading references for genus " + str(tax_id)):
+        for i, accession in tqdm(enumerate(accession_list)):
             self._download_reference(accession, directory, str(i))
-
-        #print(glob.glob(directory + "/*.fna"))
-
-        res = self.signature.find_consensus(files=glob.glob(directory + "/*.fna"))
-        self._remove_directory(directory)
-
-        return tax_id, res
+            
     
-    def find_all_signatures(self, metadata_df, num_samples=None):
+    def download_all_references(self, metadata_df, directory, num_samples=None, level="genus"):
         """
         Given a metadata_df with columns "species_taxid", "genus_taxid", "ncbi_genbank_assembly_accession"
-        find the signature for all genera.
+        download the references for all species/genera/families.
 
         For each genera, only take `num_samples` species to speed things up.
         """
-        res = {}
-        family_set = set(metadata_df.dropna(subset=['family_taxid'])['family_taxid'])
+        if level not in ["species", "genus", "family"]:
+            print("[ERROR]\t\tThe level parameter must be one of `species`, `genus` or `family`.")
+            return
+        
+        key = level + "_taxid"
+
+        # The set of all species/genera/families
+        all_set = set(metadata_df.dropna(subset=[key])[key])
         argument_list = []
-        for i, family in enumerate(family_set):
-            print("Processing family", i+1, "/", len(family_set))
-            # sample just one genome per species
-            family_df = metadata_df[metadata_df["family_taxid"] == family].groupby("species_name").sample(1)
+        for i, item in enumerate(all_set):
+            print("Processing item", i+1, "/", len(all_set))
+
+            # get the corresponding part of the metadata
+            part_df = metadata_df[metadata_df[key] == item]#.groupby("species_name").sample(1)
 
             # if there are more than `num_samples` species, sample only `num_samples` of them
-            if num_samples is not None and len(family_df) > num_samples:
-                family_df = family_df.sample(num_samples)
+            if num_samples is not None and len(part_df) > num_samples:
+                part_df = part_df.sample(num_samples)
             
             # Download the references
-            argument_list.append((family_df["ncbi_genbank_assembly_accession"],
-                                  family_df["species_taxid"], family))
-            #signature = self.find_signature(genus_df["ncbi_genbank_assembly_accession"],
-            #                                genus_df["species_taxid"], genus)
-            #res[genus] = signature
-        #return res
+            argument_list.append((part_df["ncbi_genbank_assembly_accession"], directory + str(int(item))))
+
 
         # Use multiprocessing
         print("Using", os.cpu_count(), "CPUs.")
         pool = Pool(os.cpu_count())
-        res = pool.starmap(self.find_signature, argument_list)
+        res = pool.starmap(self.download_reference_from_acession_list, argument_list)
         
         return res
+
+
+if __name__ == "__main__":
+    d = ReferenceGenomeDownloader()
+    
+    # Read metadata
+    metadata_df = pd.read_csv("/home/zhenhao/TDT/gtdb_utils/metadata_sample.csv")
+    d.download_all_references(metadata_df, "/home/zhenhao/ETFMH/data_temp/", num_samples=10, level="family")
