02f4c1408ea13a8a3ceeb4a48c459512eb0d9de7
Gu Zhenhao <guzh@comp.nus.edu.sg>
2024-06-19 16:11:48 +0800

    K-mer selection by checking information gain

---
+++ db_sketching/kmer_selection.py
@@ -0,0 +1,49 @@
+from db_sketching.kmer_set import KMerSet
+from collections import Counter
+import numpy as np
+
+class KMerImportanceLearning:
+    def __init__(self, kmer_set : KMerSet) -> None:
+        self.kmer_set = kmer_set
+        self.counter_dict = {}
+        self.label_dict = Counter()
+
+    def insert_reference(self, reference_file, label):
+        self.kmer_set.reset()
+        self.kmer_set.insert_file(reference_file)
+
+        if label not in self.counter_dict:
+            self.counter_dict[label] = Counter(set(self.kmer_set.set.keys()))
+        else:
+            self.counter_dict[label] += Counter(set(self.kmer_set.set.keys()))
+        
+        self.label_dict[label] += 1
+
+    
+    def conditional_entropy(self, kmer: int):
+        """
+        Find the conditional entropy given a kmer.
+        """
+        positive_count = []
+        negative_count = []
+
+        for label in self.label_dict:
+            # Count how many references contain this kmer
+            positive_count.append(self.counter_dict[label][kmer])
+            negative_count.append(self.label_dict[label] - self.counter_dict[label][kmer])
+
+        positive_count = np.array(positive_count)
+        negative_count = np.array(negative_count)
+
+        positive_probs = positive_count / np.sum(positive_count)
+        positive_entropy = np.sum(positive_probs * np.log(positive_probs))
+
+        negative_probs = positive_count / np.sum(positive_count)
+        negative_entropy = np.sum(negative_probs * np.log(negative_probs))
+
+        positive_rate = np.sum(positive_count) / np.sum(positive_count + negative_count)
+        res = positive_entropy * positive_rate + negative_entropy * (1-positive_rate)
+        return res
+    
+
+
