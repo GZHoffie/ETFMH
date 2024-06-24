a420bcb2492c1cce457a9e93003b0624bce8af67
Gu Zhenhao <guzh@comp.nus.edu.sg>
2024-06-24 15:12:58 +0800

    [WIP] K-mer selection methods

--- db_sketching/kmer_selection.py
+++ db_sketching/kmer_selection.py
@@ -2,8 +2,22 @@ from db_sketching.kmer_set import KMerSet
 from collections import Counter
 import numpy as np
 
-class KMerImportanceLearning:
+class KMerSelection:
+    def __init__(self):
+        pass
+
+    def select_kmers(self, **kwargs):
+        """
+        Return a dict of k-mers that are helpful based on the scheme.
+        """
+        pass
+
+class KMerInformationGain(KMerSelection):
+    """
+    Select the set of k-mers that have high information gain.
+    """
     def __init__(self, kmer_set : KMerSet) -> None:
+        super().__init__()
         self.kmer_set = kmer_set
         self.counter_dict = {}
         self.label_dict = Counter()
@@ -51,7 +65,7 @@ class KMerImportanceLearning:
         res = positive_entropy * positive_rate + negative_entropy * (1-positive_rate)
         return res
     
-    def extract_informative_kmers(self, information_gain_threshold = 0):
+    def select_kmers(self, **kwargs):
         res = dict()
 
         all_kmers = set()
@@ -63,11 +77,100 @@ class KMerImportanceLearning:
 
         for kmer in all_kmers:
             information_gain = orig_entropy - self.conditional_entropy(kmer)
-            if information_gain > information_gain_threshold:
+            if kwargs["threshold"] is not None and information_gain > kwargs["threshold"]:
                 res[kmer] = information_gain
         
         return res
+
+
+class KMerVariance(KMerInformationGain):
+    """
+    Select the k-mers that have high variance.
+    """
+    def __init__(self, kmer_set: KMerSet) -> None:
+        super().__init__(kmer_set)
     
+    def variance(self, kmer):
+        positive_count = []
+        negative_count = []
+
+        for label in self.label_dict:
+            # Count how many references contain this kmer
+            positive_count.append(self.counter_dict[label][kmer])
+            negative_count.append(self.label_dict[label] - self.counter_dict[label][kmer])
+        
+        positive_count = np.array(positive_count)
+        negative_count = np.array(negative_count)
+
+        p = positive_count / np.sum(positive_count + negative_count)
+        return np.var(p)
+
+
+    def select_kmers(self, **kwargs):
+        res = dict()
+
+        all_kmers = set()
+        for label in self.label_dict:
+            all_kmers.update(self.counter_dict[label].keys())
+
+        for kmer in all_kmers:
+            variance = self.variance(kmer)
+            if kwargs["threshold"] is not None and variance > kwargs["threshold"]:
+                res[kmer] = variance
+
+        return res
+
+class KMerChiSquaredTest(KMerInformationGain):
+    """
+    Do a Chi-squared test on the dependency of classification output on presence of k-mer.
+    """
+    def __init__(self, kmer_set: KMerSet) -> None:
+        super().__init__(kmer_set)
+    
+    def chi_squared_test(self, kmer):
+        from scipy.stats import chi2
+
+        positive_count = []
+        negative_count = []
+
+        for label in self.label_dict:
+            # Count how many references contain this kmer
+            positive_count.append(self.counter_dict[label][kmer])
+            negative_count.append(self.label_dict[label] - self.counter_dict[label][kmer])
+        
+        # Observed counts
+        positive_count = np.array(positive_count)
+        negative_count = np.array(negative_count)
+        total_count = positive_count + negative_count
+
+        # Expected Counts
+        positive_rate = np.sum(positive_count) / np.sum(total_count)
+        positive_expected = positive_rate * total_count
+        negative_expected = (1-positive_rate) * total_count
+
+        # Do chi-squared test
+        degree_of_freedom = len(positive_count) - 1
+        statistics = (positive_count - positive_expected) ** 2 / positive_expected + \
+                     (negative_count - negative_expected) ** 2 / negative_expected
+        p_value = 1 - chi2(degree_of_freedom).cdf(statistics)
+
+        return p_value
+
+
+    def select_kmers(self, **kwargs):
+        res = dict()
+
+        all_kmers = set()
+        for label in self.label_dict:
+            all_kmers.update(self.counter_dict[label].keys())
+
+        for kmer in all_kmers:
+            p_value = self.chi_squared_test(kmer)
+            if kwargs["threshold"] is not None and p_value <= kwargs["threshold"]:
+                res[kmer] = p_value
+
+        return res
+
 
 
 if __name__ == "__main__":
